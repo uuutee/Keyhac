ec76862e63b5d7bc1fefc5d94c98a0ebbddbf1c9
uuutee <uuuteenet@gmail.com>
2017-01-14 02:13:24 +0900

    Add Cursor keys

--- config.py
+++ config.py
@@ -43,15 +43,10 @@ def configure(keymap):
         keymap_global[ "Ctrl-Alt-Up"    ] = keymap.MoveWindowCommand( 0, -10 )
         keymap_global[ "Ctrl-Alt-Down"  ] = keymap.MoveWindowCommand( 0, +10 )
 
-        # Ctrl-Alt-Shift-Up/Down/Left/Right : Move active window by 1 pixel unit
-        keymap_global[ "Ctrl-Alt-Shift-Left"  ] = keymap.MoveWindowCommand( -1, 0 )
-        keymap_global[ "Ctrl-Alt-Shift-Right" ] = keymap.MoveWindowCommand( +1, 0 )
-        keymap_global[ "Ctrl-Alt-Shift-Up"    ] = keymap.MoveWindowCommand( 0, -1 )
-        keymap_global[ "Ctrl-Alt-Shift-Down"  ] = keymap.MoveWindowCommand( 0, +1 )
-
-        # Ctrl-Alt-Cmd-Up/Down/Left/Right : Move active window to screen edges
-        keymap_global[ "Ctrl-Alt-Cmd-Left"  ] = keymap.MoveWindowToMonitorEdgeCommand(0)
-        keymap_global[ "Ctrl-Alt-Cmd-Right" ] = keymap.MoveWindowToMonitorEdgeCommand(2)
-        keymap_global[ "Ctrl-Alt-Cmd-Up"    ] = keymap.MoveWindowToMonitorEdgeCommand(1)
-        keymap_global[ "Ctrl-Alt-Cmd-Down"  ] = keymap.MoveWindowToMonitorEdgeCommand(3)
-
+    if 1:
+        # Cursor keys
+        keymap_global[ "Ctrl-P" ] = "Up"    
+        keymap_global[ "Ctrl-N" ] = "Down"  
+        keymap_global[ "Ctrl-F" ] = "Right"  
+        keymap_global[ "Ctrl-B" ] = "Left"  
+        keymap_global[ "Ctrl-J" ] = "Enter" 
